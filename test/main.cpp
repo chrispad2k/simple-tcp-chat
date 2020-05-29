183400a4a17bee098d287e6c71bee9c2e3108821
Christoph Pader <chrispad2k@gmail.com>
2020-05-29 15:27:56 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Christoph Pader on 29.05.20.
+//  Copyright © 2020 Christoph Pader. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
