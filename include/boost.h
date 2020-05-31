49b9412a40e615298a0c840428c247ab46b77a78
Christoph Pader <chrispad2k@gmail.com>
2020-05-31 17:22:53 +0200

    Delete boost.h

--- include/boost.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
-#include <boost/asio.hpp>
-#include <boost/array.hpp>
-
-using boost::asio::ip::tcp;
